f8d9efb345dc83934bd793f821c9c3b0943ad6a3
杨奕 <leopoldthecuber@gmail.com>
2015-09-25 18:48:38 +0800

    syntax corrected

--- src/pagination/index.js
+++ src/pagination/index.js
@@ -13,20 +13,22 @@ class Pagination {
   }
 
   updateState(currentPage, pageCount, ul) {
-    this.resetUl(ul);
     var self = this;
     var newList = [];
     var flag = [];
     var newPage = 0;
+    var i = 0;
+    var len = 0;
+    var li;
     var bindEvent = function(i, li) {
-      if (li.innerHTML !== '...') {
+      if (li.className !== 'ellipsis') {
         li.addEventListener('click', function() {
           newPage = 0;
-          if (this.innerHTML === '&lt;') {
+          if (this.className === 'prevBtn') {
             if (currentPage > 1) {
               newPage = currentPage - 1;
             }
-          } else if (this.innerHTML === '&gt;') {
+          } else if (this.className === 'nextBtn') {
             if (currentPage < pageCount) {
               newPage = currentPage + 1;
             }
@@ -39,60 +41,64 @@ class Pagination {
               self.onChange(newPage);
             }
           }
-        })
+        });
+      }
+    };
+    var resetUl = function() {
+      for (i = 0, len = ul.childNodes.length; i < len; i++) {
+        ul.removeChild(ul.childNodes[0]);
       }
     };
     if (pageCount < 6) {
-      for (var i = 0; i < pageCount; i++) {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(i + 1));
+      for (i = 0; i < pageCount; i++) {
+        li = document.createElement('li');
+        li.appendChild(document.createTextNode((i + 1).toString()));
         newList.push(li);
         bindEvent(i ,li);
       }
     } else {
       if (currentPage <= 3) {
-        flag = ["<", "1", "2", "3", "4", "...", pageCount.toString(), ">"];
+        flag = ['<', '1', '2', '3', '4', '...', pageCount.toString(), '>'];
       } else if (currentPage >= pageCount - 2) {
-        flag = ["<", "1", "...", (pageCount - 3).toString(), (pageCount - 2).toString(), (pageCount - 1).toString(), pageCount.toString(), ">"];
+        flag = ['<', '1', '...', (pageCount - 3).toString(), (pageCount - 2).toString(), (pageCount - 1).toString(), pageCount.toString(), '>'];
       } else {
-        flag = ["<", "1", "..."];
+        flag = ['<', '1', '...'];
         for (i = -1; i < 2; i++) {
           flag.push((currentPage + i).toString());
         }
-        flag.push("...");
+        flag.push('...');
         flag.push(pageCount.toString());
-        flag.push(">");
+        flag.push('>');
       }
-      for (var len = flag.length, i = 0; i < len; i++) {
-        var li = document.createElement("li");
+      for (i = 0, len = flag.length; i < len; i++) {
+        li = document.createElement('li');
         li.appendChild(document.createTextNode(flag[i]));
         if (flag[i] === '...') {
-          li.className = "ellipsis";
+          li.className = 'ellipsis';
+        } else if (flag[i] === '<') {
+          li.className = 'prevBtn';
+        } else if (flag[i] === '>') {
+          li.className = 'nextBtn';
         }
         newList.push(li);
         bindEvent(i, li);
       }
     }
-    
-    for (var i = 0, len = newList.length; i < len; i++) {
-      if (newList[i].className === "active") {
-        newList[i].className = "";
+    resetUl(ul);
+    for (i = 0, len = newList.length; i < len; i++) {
+      if (newList[i].className === 'active') {
+        newList[i].className = '';
       }
       if (parseInt(newList[i].innerHTML) === currentPage) {
-        newList[i].className = "active";
+        newList[i].className = 'active';
       }
       ul.appendChild(newList[i]);
     }
-  };
-
-  resetUl(ul) {
-    for (var i = 0, len = ul.childNodes.length; i < len; i++) {
-      ul.removeChild(ul.childNodes[0]);
-    }
   }
 
+
   init() {
-    var ul = document.createElement("ul");
+    var ul = document.createElement('ul');
     this.updateState(this.currentPage, this.pageCount, ul);
     return ul;
   }
@@ -104,6 +110,6 @@ class Pagination {
     element.appendChild(this.init());
     parent.appendChild(element);
   }
-};
+}
 
 module.exports = Pagination;
