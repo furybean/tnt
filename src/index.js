88a286bc0d10cd78fdedde9cf0eda3f8b86cfec2
furybean <long.zhang@ele.me>
2015-10-07 01:27:46 +0800

    Add Popup mixin. Add MsgBox.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('./css/iconfont.css');
+require('./util');
 
 var tnt = {
   Pagination: require('./pagination'),
   notification: require('./notification'),
   Alert: require('./alert'),
-  DatePicker: require('./datepicker')
+  DatePicker: require('./datepicker'),
+  MessageBox: require('./msgbox/msgbox'),
+  Popup: require('./popup')
 };
 
 window.tnt = tnt;
