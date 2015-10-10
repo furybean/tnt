6708c104b4da509d68e82d89155e93b7fa9ff345
furybean <long.zhang@ele.me>
2015-10-10 12:37:29 +0800

    Add MsgBox Icon.

--- src/datepicker/index.js
+++ src/datepicker/index.js
@@ -267,9 +267,9 @@ class DatePicker extends Component {
     return (<div class="datepicker">
 
       <div class="datepicker-header">
-        <button onclick={ self.prev.bind(self) } class="datepicker-prevbtn icon-arrow-left"></button>
+        <button onclick={ self.prev.bind(self) } class="datepicker-prevbtn iconfont icon-datepicker-left-arrow"></button>
         <label onclick={ self.handleLabelClick.bind(self) }>{ label }</label>
-        <button onclick={ self.next.bind(self) } class="datepicker-nextbtn icon-arrow-right"></button>
+        <button onclick={ self.next.bind(self) } class="datepicker-nextbtn iconfont icon-datepicker-right-arrow"></button>
       </div>
       <div class="datepicker-body">
         <table class={ self.currentView === 'date' ? '' : 'hidden' } onclick={ self.handleDateTableClick.bind(self) }>
